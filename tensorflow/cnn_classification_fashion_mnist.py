26961548037357b034220599566302cf596186b3
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2020-04-15 17:22:11 -0400

    Create cnn_classification_fashion_mnist.py

---
+++ tensorflow/cnn_classification_fashion_mnist.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+
+# read in the data
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print("x_train.shape:", x_train.shape)
+
+# the data is only 2D!
+# convolution expects height x width x color
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print(x_train.shape)
+
+# number of classes
+k = len(set(y_train))
+print(f"number of classes: {k}")
+
+# Build the model using the functional API
+i = Input(shape=x_train[0].shape)
+x = Conv2D(32, (3, 3), strides=2, activation='relu')(i)
+x = Conv2D(64, (3, 3), strides=2, activation='relu')(x)
+x = Conv2D(128, (3, 3), strides=2, activation='relu')(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
