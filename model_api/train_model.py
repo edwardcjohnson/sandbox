e6c2210bce0817582ce184a8c82cbc61a55e0e27
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2023-05-15 00:00:55 -0400

    Update train_model.py

--- model_api/train_model.py
+++ model_api/train_model.py
@@ -1,9 +1,14 @@
+import argparse
 import pandas as pd
 import lightgbm as lgb
 import numpy as np
 
-def train_model():
+def train_model(dataset_path: str, model_name: str):
     """Trains a LightGBM regression model on a synthetic dataset and saves it to disk.
+
+    Args:
+        dataset_path (str): Path to the synthetic dataset
+        model_name (str): Name of the file to save the trained model
 
     Returns:
         None
@@ -12,10 +17,10 @@ def train_model():
         None
 
     Example:
-        >>> train_model()
+        >>> train_model('synthetic_data.csv', 'model.txt')
     """
-    # Create the synthetic dataset
-    df = create_synthetic_data()
+    # Load the synthetic dataset
+    df = pd.read_csv(dataset_path)
 
     # Split the data into features (X) and target (y)
     X = df.drop(columns=['target'])
@@ -30,47 +35,15 @@ def train_model():
     model = lgb.train(params, train_data)
 
     # Save the trained model to disk
-    model.save_model('model.txt')
-
-
-def create_synthetic_data(n_samples=1000):
-    """Create a synthetic dataset with specified number of samples and features.
-
-        Args:
-            n_samples (int, optional): Number of samples in the dataset. Defaults to 1000.
-
-        Returns:
-            pandas.DataFrame: A synthetic dataset containing random values for each feature and a target variable
-            generated using a linear combination of the features.
-
-        Raises:
-            ValueError: If n_samples is not a positive integer.
-            ValueError: If feature_ranges is not a list of tuples containing the low and high range for each feature.
-    """
-    # Define the number of features
-    n_features = 3
-    
-    # Define the range of values for each feature
-    feature_ranges = [(0, 1), (0, 1), (0, 1)]
-    
-    # Generate random values for each feature
-    X = np.random.uniform(
-        low=[r[0] for r in feature_ranges],
-        high=[r[1] for r in feature_ranges],
-        size=(n_samples, n_features)
-    )
-    
-    # Generate the target variable using a linear combination of the features
-    beta = np.array([0.5, 0.3, 0.2])
-    error = np.random.normal(scale=0.1, size=n_samples)
-    y = np.dot(X, beta) + error
-    
-    # Combine the features and target variable into a DataFrame
-    data = pd.DataFrame(X, columns=[f'feature_{i+1}' for i in range(n_features)])
-    data['target'] = y
-    
-    return data
+    model.save_model(model_name)
 
 
 if __name__ == '__main__':
-    train_model()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Train a LightGBM regression model on a synthetic dataset')
+    parser.add_argument('--dataset_path', type=str, default='synthetic_data.csv', help='Path to the synthetic dataset')
+    parser.add_argument('--model_name', type=str, default='model.txt', help='Name of the file to save the trained model')
+    args = parser.parse_args()
+
+    # Train the model
+    train_model(args.dataset_path, args.model_name)
