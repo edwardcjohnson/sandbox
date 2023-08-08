b234f9a7e8d3aa79c3b0938c00a5720149894599
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2023-08-08 10:08:33 -0400

    Update doc_qa.py

--- generative_ai/langchain/doc_qa.py
+++ generative_ai/langchain/doc_qa.py
@@ -1,5 +1,10 @@
 # https://python.langchain.com/docs/use_cases/question_answering.html
-from langchain.document_loaders import UnstructuredPDFLoader# PDFMinerLoader#UnstructuredPDFLoader # PyPDFLoader
+from langchain.llms import OpenAI
+from langchain.chains import (
+    StuffDocumentsChain, LLMChain, ConversationalRetrievalChain
+)
+# PDFMinerLoader#UnstructuredPDFLoader # PyPDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -7,70 +12,47 @@ from langchain.chat_models import ChatOpenAI
 # from langchain.chains import RetrievalQA
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.prompts import PromptTemplate
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+import pprint
 import os
 
-
-# Step 1. Load
-loader = UnstructuredPDFLoader("./data/example.pdf")
-data = loader.load()
-# pages = loader.load_and_split()
-
-# Step 2. Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
-doc_splits = text_splitter.split_documents(data)
-
-# Step 3. Store
-vectorstore = Chroma.from_documents(
-    documents=doc_splits, embedding=OpenAIEmbeddings(), 
+# Step 1. Load vectorstore
+vectorstore = Chroma(
     persist_directory="./data/chroma_db",
-    collection_name="hr_collection"
+    collection_name="test_collection",
+    embedding_function=OpenAIEmbeddings()
 )
-# ---- DB interaction: https://docs.trychroma.com/api-reference
-# import chromadb
-# client = chromadb.PersistentClient(path="src/data/chroma_db")
-# client.list_collections()
-# client.delete_collection("<collection_name>")
-#---------------------------
 
-# Step 4. Retrieve
-question = "summarize the document in 3 bulletpoints?"
-docs = vectorstore.similarity_search(question)
-len(docs)
+# Step 2. Generate
+# streaming_llm = OpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=0)
 
-# Step 5. Generate
+# libs/langchain/langchain/chains/conversational_retrieval/prompts.py
+template = """You are a friendly assistant. Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 
-template = """Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-Use five sentences maximum and keep the answer as concise as possible. 
-{context}
-Question: {question}
-Helpful Answer:"""
-# `from_template` method will automatically infer the input_variables based on the template passed.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+
 prompt = PromptTemplate.from_template(template)
-
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
-    llm,
-    retriever=vectorstore.as_retriever(),
-    chain_type="stuff", # "stuffs" all retrieved documents into the prompt.
-    # chain_type_kwargs={"prompt": prompt}
-    verbose=True
+from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
+question_generator_chain = LLMChain(llm=llm, prompt=prompt) # prompt=prompt; CONDENSE_QUESTION_PROMPT
+doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff")
+
+chain = ConversationalRetrievalChain(
+    retriever=vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 4}),
+    question_generator=question_generator_chain,
+    combine_docs_chain=doc_chain,
 )
-result = qa_chain({"question": question})
-result
 
-# Step 6. Converse (Extension)
-from langchain.memory import ConversationBufferMemory
+chat_history = []
+question = "what question should i ask you?"
+result = chain({"question": question, "chat_history": chat_history})
+pprint.pprint(result)
 
-buffer_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, k=2)
+chat_history = [(question, result['answer'])]
+question = "What other questions should i ask you?"
+result = chain({"question": question, "chat_history": chat_history})
+pprint.pprint(result)
 
-qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
-    llm,
-    retriever=vectorstore.as_retriever(),
-    chain_type="stuff", # "stuffs" all retrieved documents into the prompt.
-    memory=buffer_memory,
-    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-    verbose=True
-)
-result = qa_chain({"query": question})
-result
